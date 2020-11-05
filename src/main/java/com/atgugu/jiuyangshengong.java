09efce7741534fcb6d8664138a2d446efbdda95e
zkz <3031361355@qq.com>
2020-11-05 11:46:20 +0800

    Initial commit

---
+++ src/main/java/com/atgugu/jiuyangshengong.java
@@ -0,0 +1,11 @@
+package com.atgugu;
+
+/**
+ * @author shkstart
+ * @create 2020-11-05 11:25
+ */
+public class jiuyangshengong {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
